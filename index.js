b0a2ffe005028a21561d35fef0eeb50fff7c63cb
Andrew Walters <andrewjpwalters@gmail.com>
2022-04-20 01:22:19 -0500

    commit tree ec725ad45842a08a32eb784059b534bffd53e50a

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === word.split("").reverse().join("")) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  if word === word, split, reversed and then joined
+  then return true
+  else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  Using built-in JS methods, the provided string is compared to itself
+  split into an array, reversed and joined back together in a string.
+  If this is true, then true is returned.
+  If this is not true, then false is returned.
 */
 
 // You can run `node index.js` to view these console logs
